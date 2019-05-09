1a3f2022b76258c6116f249abbd358879681aad8
Thomas May <wamwoowam@gmail.com>
2019-05-09 22:08:42 +0100

    Make it work.
    - Make `instanceof` check actually work
    - Support setting JS properties and HTML elements

--- module.js
+++ module.js
@@ -1,17 +1,25 @@
-export default function(tag, attrs, ...children) {
+export default function (tag, attrs, ...children) {
   let elem = document.createElement(tag);
 
   if (attrs === null || attrs === undefined) attrs = {};
   for (let [attr, value] of Object.entries(attrs)) {
-    if (value === true) elem.setAttribute(attr, attr);
-    else if (value !== false && value !== null) {
-      elem.setAttribute(attr, value.toString());
+
+    if (elem.hasAttribute(attr)) {
+      if (value === true) {
+        elem.setAttribute(attr, attr);
+      }
+      else if (value !== false && value !== null) {
+        elem.setAttribute(attr, value);
+      }
+    }
+    else if (attr in elem){
+      elem[attr] = value;
     }
   }
 
   for (let child of children) {
-    if (! child instanceof Node) {
-      child = document.createTextNode(child.toString());
+    if (!(child instanceof Node)) {
+      child = document.createTextNode(child);
     }
     elem.appendChild(child);
   }
