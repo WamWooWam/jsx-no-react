fc7be92b5aecd7707978df7d051162de2ed88900
Thomas May <wamwoowam@gmail.com>
2019-07-10 08:41:00 +0100

    Arrays?

--- module.js
+++ module.js
@@ -1,18 +1,19 @@
 export default function (tag, attrs, ...children) {
   let elem = document.createElement(tag);
 
-  if (attrs === null || attrs === undefined) attrs = {};
-  for (let [attr, value] of Object.entries(attrs)) {
-    if (elem.hasAttribute(attr)) {
-      if (value === true) {
-        elem.setAttribute(attr, attr);
+  if (attrs !== null && attrs !== undefined) {
+    for (let [attr, value] of Object.entries(attrs)) {
+      if (attr in elem) {
+        elem[attr] = value;
       }
-      else if (value !== false && value !== null) {
-        elem.setAttribute(attr, value);
+      else {
+        if (value === true) {
+          elem.setAttribute(attr, attr);
+        }
+        else if (value !== false && value !== null) {
+          elem.setAttribute(attr, value);
+        }
       }
-    }
-    else if (attr in elem) {
-      elem[attr] = value;
     }
   }
 
@@ -20,11 +21,20 @@ export default function (tag, attrs, ...children) {
     if (child === undefined) {
       continue;
     }
+    if (child instanceof Array){
+      for(let child2 of child){
+        appendChild(child2);
+      }
+    }    
+    appendChild(child);
+  }
+
+  return elem;
+
+  function appendChild(child) {
     if (!(child instanceof Node)) {
       child = document.createTextNode(child);
     }
     elem.appendChild(child);
   }
-
-  return elem;
 };
